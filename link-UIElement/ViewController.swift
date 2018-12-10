1e5e2c33e1b2036cd8b3416bee31639bf77d744f
Stephen Ouyang <xisouyang@gmail.com>
2018-12-10 15:00:17 -0800

    first commit, able to see tableView

--- link-UIElement/ViewController.swift
+++ link-UIElement/ViewController.swift
@@ -6,15 +6,45 @@
 //  Copyright © 2018 Stephen Ouyang. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+// Must add tableView Delegate and Data Source
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableViewData: [String] = []
+    let string = "Hello World"
+    
+    
+    //returns the amount of rows the tableView will have
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+        
+    }
+    
+    //returns the cell itself
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier") as! aTableViewCell
+        let text = tableViewData[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Must set delegate and dataSource for tableView to show changes
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        for i in 0...10 {
+            tableViewData.append(string)
+        }
     }
-
-
 }
 
---
+++ link-UIElement/aTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  aTableViewCell.swift
+//  link-UIElement
+//
+//  Created by Stephen Ouyang on 12/10/18.
+//  Copyright © 2018 Stephen Ouyang. All rights reserved.
+//
+
+import UIKit
+
+
+//class must exist for the cell, cell's class name must be the same as this class
+class aTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
